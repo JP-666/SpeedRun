6af5c19079e2dcf57d5e4c0a5ba229e45b94c587
JP <jcargue0501@g.educaand.es>
2025-11-19 14:25:43 +0100

    Cambiar el metodo de composicion de mensajes para solo usar un array
    Tambien se han añadido algunos colores a los mensajes, esto requiere usar un RichTextLabel, con el BBCode

--- Princ.gd
+++ Princ.gd
@@ -1,15 +1,28 @@
 extends Control
 
 var Mensajes = JSON.parse_string(FileAccess.get_file_as_string("res://mensajes.json"))
-
+var Colores = [
+	"#FF5733",  # Rojo cor
+	"#33FF57",  # Verde neon
+	"#3357FF",  # Azul elec
+	"#F3FF33",  # Amarillo bri
+	"#FF33F3",  # Magenta vibr
+	"#33FFF3",  # Cian aguamar
+	"#A033FF",  # Púrpura intenso
+	"#FFB833",  # Naranja cal
+	"#33FFB8",  # Menta
+	"#B833FF"   # Violeta
+]
 
 func EnviarChat() -> void:
-	print("Ej")
+	
 	var Mensaje : String = ""
+	Mensaje+=str("[color=",Colores.pick_random(),"]")
 	Mensaje+=Mensajes["NombresUsuarioPARTE1"].pick_random()
 	Mensaje+=Mensajes["NombresUsuarioPARTE2"].pick_random()
 	Mensaje+=Mensajes["NombresUsuarioPARTE3"].pick_random()
-	Mensaje+=": "
-	Mensaje+=Mensajes[["ArrayMensajes","ArrayMensajesNegativos","ArrayMensajesRaros"].pick_random()].pick_random()
+	Mensaje+="[/color]: "
+	Mensaje+=Mensajes["ArrayMensajes"].pick_random()
 	Mensaje+="\n"
-	$CajaTwichChat/TwichChat.text+=Mensaje
+	$CajaTwichChat/SC/TwichChat.text+=Mensaje
+	$CajaTwichChat/SC.scroll_vertical=-1
